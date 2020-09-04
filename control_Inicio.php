8f7927a0692fd5e266fbc447327203c3ee0b2701
Luz-yo <luzyovm@gmail.com>
2020-09-03 19:16:47 -0500

    Cambios identacion e boton bingo

--- control_Inicio.php
+++ control_Inicio.php
@@ -1,32 +1,27 @@
 <?php
 
-session_start();
-require_once 'Model/Bingo.php';
-$objBingo = new Bingo();
+    session_start();
+    require_once 'Model/Bingo.php';
+    $objBingo = new Bingo();
 
+    if ( isset( $_POST['Jugar'] ) )  {
+        $idTipo = $_POST['cboTipo'];
+        $nombre = $_POST['nombrePartida'];
 
+        $id = $objBingo->iniciarBingo( $nombre, $idTipo, $_SESSION['idUsuario'] );
+        $_SESSION['idBingo'] = $id;
 
-if(isset($_POST['Jugar'])) 
-{
-  $idTipo = $_POST['cboTipo'];
-  $nombre = $_POST['nombrePartida'];
+        header( "location: bingo.html?idJuego=$id-nombre_Partida=$nombre-tipo=$idTipo" );
 
-  $id = $objBingo->iniciarBingo($nombre, $idTipo, $_SESSION["idUsuario"]);
-  $_SESSION["idBingo"] = $id;
+    } else if ( isset( $_POST['unirse'] ) ) {
 
-  header("location: bingo.html?idJuego=$id-nombre_Partida=$nombre-tipo=$idTipo");
+        $id = $_POST['cboJuegosActivos'];
+        $_SESSION['idBingo'] = $id;
+        $objBingo->conectarJugador( $id, $_SESSION['idUsuario'] );
 
-}else if(isset($_POST["unirse"]))
-{
-
-$id = $_POST["cboJuegosActivos"];
-$_SESSION["idBingo"] = $id;
-$objBingo->conectarJugador($id,$_SESSION["idUsuario"]);
-
-header("location:multijugador_bingo.html?idJuego=$id");
-  }else {
-  	header("location:inicio.php");
-  }
-
-
+        header( "location:multijugador_bingo.html?idJuego=$id" );
+    } else {
+        header( 'location:inicio.php' );
+    }
+    
 ?>
